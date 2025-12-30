8c3256d0e337662c90ee0ee4a8f661236e8e54dc
NIcolasCalzolariRupo <nicocalzolari05@gmail.com>
2025-12-30 13:15:04 -0300

    Create ItemCount.jsx

---
+++ ItemCount.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function ItemCount({ stock, onAdd }) {
+  const [count, setCount] = useState(1);
+
+  return (
+    <div>
+      <button onClick={() => count > 1 && setCount(count - 1)}>-</button>
+      <span>{count}</span>
+      <button onClick={() => count < stock && setCount(count + 1)}>+</button>
+      <button onClick={() => onAdd(count)}>Agregar</button>
+    </div>
+  );
+}
